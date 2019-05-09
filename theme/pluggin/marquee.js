c29788a17c0e82d46cd13b3f95ea3a2fd148c216
luuquangtri <48348502+luuquangtri@users.noreply.github.com>
2019-05-09 18:10:09 +0700

    ds

---
+++ theme/pluggin/marquee.js
@@ -0,0 +1,16 @@
+//<![CDATA[
+marqueeInit({
+uniqueid: 'mycrawler',
+style: {
+'margin-top': '2px',
+'padding': '1px',
+'width': '900px',
+'height': '80px'
+},
+inc: 0, //speed - pixel increment for each iteration of this marquee's movement
+mouse: 'false', //mouseover behavior ('pause' 'cursor driven' or false)
+moveatleast: 1,
+neutral: 150,
+savedirection: true
+});
+//]]> 
